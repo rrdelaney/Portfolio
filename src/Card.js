173adf838b4427b4cb0c7c02378edb7632a5a02b
Ryan Delaney <rrdelaney@outlook.com>
2017-04-11 21:39:43 -0700

    Print media

--- src/Card.js
+++ src/Card.js
@@ -6,5 +6,9 @@ export default glamorous.div(props => ({
   minWidth: '400px',
   padding: '1rem',
   margin: props.center ? '0 auto' : '1rem',
-  boxShadow: 'rgba(0, 0, 0, .2) 2px 2px 4px 2px'
+  boxShadow: 'rgba(0, 0, 0, .2) 2px 2px 4px 2px',
+  '@media print': {
+    boxShadow: 'none',
+    border: '1px solid grey'
+  }
 }))
