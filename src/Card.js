92606100f4e67552cdd7036e77e78fe1608ae0ef
Ryan Delaney <rrdelaney@outlook.com>
2017-04-18 22:19:42 -0700

    Responsive cards

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import glamorous from 'glamorous'
 export default glamorous.div(props => ({
   backgroundColor: 'whitesmoke',
   maxWidth: '400px',
-  minWidth: '400px',
+  width: '90vw',
   padding: '1rem',
   margin: props.center ? '0 auto' : '1rem',
   boxShadow: 'rgba(0, 0, 0, .2) 2px 2px 4px 2px',
