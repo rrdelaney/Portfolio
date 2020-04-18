285625782bd78672798b4828873e8a6f16f482b5
Ryan Delaney <rrdelaney@outlook.com>
2020-04-18 16:50:59 -0700

    Min/max windows [skip ci]

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,67 @@
 import * as React from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faTvRetro, IconDefinition} from '@fortawesome/pro-duotone-svg-icons';
+import {
+  faTvRetro,
+  IconDefinition,
+  faStarHalf,
+  faWindowClose,
+  faWindowMinimize,
+  faWindowMaximize,
+} from '@fortawesome/pro-duotone-svg-icons';
 
 const INVISIBLE_IMG =
   'data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=';
 
 const WINDOW_BAR_HEIGHT = 20;
+const BOTTOM_BAR_HIGHT = WINDOW_BAR_HEIGHT * 2;
+
+type Dimension = [number, number];
+
+function useWindowDimensions(): Dimension {
+  if (typeof window === 'undefined') return [0, 0];
+
+  const [dimensions, setDimensions] = React.useState([0, 0] as Dimension);
+
+  React.useLayoutEffect(() => {
+    const handleResize = () => {
+      setDimensions([window.innerWidth, window.innerHeight]);
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return dimensions;
+}
 
 interface WindowProps {
   title: string;
+  icon: IconDefinition;
+  shown: boolean;
   initX?: number;
   initY?: number;
   onClose?: () => void;
+  onMinimize?: () => void;
 }
 
 const Window: React.FC<WindowProps> = ({
   initX = 0,
   initY = 0,
   title,
+  icon,
+  shown,
   onClose,
+  onMinimize,
   children,
 }) => {
-  const [x, setX] = React.useState(initX);
-  const [y, setY] = React.useState(initY);
-  const [diff, setDiff] = React.useState([0, 0] as [number, number]);
+  const dimensions = useWindowDimensions();
+  const [position, setPosition] = React.useState([initX, initY] as Dimension);
+  const [diff, setDiff] = React.useState([0, 0] as Dimension);
+  const innerWindowRef = React.useRef<HTMLDivElement | null>(null);
 
   const dragImage = React.useMemo(() => {
     if (typeof window === 'undefined') return null;
@@ -37,52 +75,86 @@ const Window: React.FC<WindowProps> = ({
     (e: React.DragEvent<HTMLDivElement>) => {
       e.dataTransfer.setDragImage(dragImage!, 0, 0);
 
-      setDiff([e.clientX - x, e.clientY - y]);
+      setDiff([e.clientX - position[0], e.clientY - position[1]]);
     },
-    [dragImage, x, y]
+    [dragImage, position]
   );
 
   const handleDrag = React.useCallback(
     (e: React.DragEvent<HTMLDivElement>) => {
       if (e.screenX === 0 && e.screenY === 0) return false;
 
-      setX(e.clientX - diff[0]);
-      setY(e.clientY - diff[1]);
+      setPosition([e.clientX - diff[0], e.clientY - diff[1]]);
     },
     [diff]
   );
 
-  const handleDragEnd = React.useCallback(
-    (e: React.DragEvent<HTMLDivElement>) => {},
-    []
-  );
+  const handleMaximize = React.useCallback(() => {
+    setPosition([0, 0]);
+    innerWindowRef.current!.style.width = `${dimensions[0]}px`;
+    innerWindowRef.current!.style.height = `${
+      dimensions[1] - BOTTOM_BAR_HIGHT - WINDOW_BAR_HEIGHT
+    }px`;
+  }, [innerWindowRef, dimensions]);
 
   return (
     <div
       style={{
-        transform: `translate(${x}px, ${y}px)`,
+        transform: `translate(${position[0]}px, ${position[1]}px)`,
         background: 'white',
         position: 'absolute',
+        display: shown ? undefined : 'none',
       }}>
       <div
         draggable="true"
         onDragStart={handleDragStart}
         onDrag={handleDrag}
-        onDragEnd={handleDragEnd}
         style={{
           height: `${WINDOW_BAR_HEIGHT}px`,
           width: '100%',
           background: 'purple',
           display: 'flex',
-          fontFamily: 'VT323',
+          alignItems: 'center',
+          fontFamily: 'Cabin',
+          fontSize: '10pt',
+          color: 'white',
         }}>
-        <span style={{flex: '1'}}>{title}</span>
-        <button onClick={onClose}>x</button>
+        <FontAwesomeIcon icon={icon} inverse style={{paddingLeft: '5px'}} />
+        <span
+          style={{
+            flex: '1',
+            paddingLeft: '5px',
+          }}>
+          {title}
+        </span>
+        <FontAwesomeIcon
+          icon={faWindowMinimize}
+          inverse
+          style={{paddingRight: '5px', cursor: 'pointer'}}
+          onClick={onMinimize}
+        />
+        <FontAwesomeIcon
+          icon={faWindowMaximize}
+          inverse
+          style={{paddingRight: '5px', cursor: 'pointer'}}
+          onClick={handleMaximize}
+        />
+        <FontAwesomeIcon
+          icon={faWindowClose}
+          inverse
+          style={{paddingRight: '5px', cursor: 'pointer'}}
+          onClick={onClose}
+        />
       </div>
       <div
+        ref={innerWindowRef}
         style={{
           overflow: 'scroll',
           resize: 'both',
+          maxWidth: '100vw',
+          maxHeight: `${
+            dimensions[1] - BOTTOM_BAR_HIGHT - WINDOW_BAR_HEIGHT
+          }px`,
         }}>
         {children}
       </div>
@@ -136,7 +208,79 @@ const DesktopShortcut: React.FC<DesktopShortcutProps> = ({
   );
 };
 
-const Desktop: React.FC = ({children}) => {
+let nextAppId = 1;
+
+interface AppProps {
+  readonly id?: number;
+  readonly title: string;
+  readonly icon: IconDefinition;
+  readonly content: string;
+  readonly shown: boolean;
+}
+
+class App implements AppProps {
+  readonly id: number;
+  readonly title: string;
+  readonly icon: IconDefinition;
+  readonly content: string;
+  readonly shown: boolean;
+
+  private constructor(props: AppProps) {
+    this.title = props.title;
+    this.icon = props.icon;
+    this.content = props.content;
+    this.shown = props.shown;
+
+    if (props.id) {
+      this.id = props.id;
+    } else {
+      this.id = nextAppId;
+      nextAppId += 1;
+    }
+  }
+
+  private props(): AppProps & {id: number} {
+    return {
+      id: this.id,
+      title: this.title,
+      icon: this.icon,
+      content: this.content,
+      shown: this.shown,
+    };
+  }
+
+  static fromContent(title: string, icon: IconDefinition, content: string) {
+    return new App({title, icon, content, shown: true});
+  }
+
+  minimize(): App {
+    return new App({...this.props(), shown: false});
+  }
+
+  show(): App {
+    return new App({...this.props(), shown: true});
+  }
+}
+
+type AppShortcut = Pick<App, 'title' | 'icon'>;
+
+interface DesktopProps {
+  apps: App[];
+  shortcuts: AppShortcut[];
+  onOpenApp?: (shortcut: AppShortcut) => void;
+  onCloseApp?: (app: App) => void;
+  onMinimizeApp?: (app: App) => void;
+  onShowApp?: (app: App) => void;
+}
+
+const Desktop: React.FC<DesktopProps> = ({
+  apps,
+  shortcuts,
+  onOpenApp,
+  onCloseApp,
+  onMinimizeApp,
+  onShowApp,
+}) => {
   return (
     <div
       style={{
@@ -149,55 +293,121 @@ const Desktop: React.FC = ({children}) => {
         gridTemplateColumns: 'repeat(10, 1fr)',
         gridTemplateRows: 'repeat(10, 1fr)',
       }}>
-      {children}
+      {apps.map((app) => {
+        return (
+          <Window
+            key={app.id}
+            title={app.title}
+            icon={app.icon}
+            shown={app.shown}
+            onClose={() => onCloseApp?.(app)}
+            onMinimize={() => onMinimizeApp?.(app)}>
+            <p>{app.content}</p>
+          </Window>
+        );
+      })}
+
+      {shortcuts.map((shortcut) => {
+        return (
+          <DesktopShortcut
+            key={shortcut.title}
+            title="Hello world"
+            icon={faTvRetro}
+            action={() => onOpenApp?.(shortcut)}
+          />
+        );
+      })}
+
+      <div
+        style={{
+          position: 'absolute',
+          height: `${WINDOW_BAR_HEIGHT * 2}px`,
+          bottom: '0',
+          width: '100vw',
+          display: 'flex',
+          alignItems: 'center',
+          background: 'purple',
+        }}>
+        <FontAwesomeIcon
+          icon={faStarHalf}
+          inverse
+          size="lg"
+          style={{padding: '.5rem'}}
+        />
+
+        {apps.map((app) => {
+          return (
+            <div
+              key={app.id}
+              onClick={() => onShowApp?.(app)}
+              style={{
+                height: '30px',
+                width: '150px',
+                border: '1px solid white',
+                display: 'flex',
+                alignItems: 'center',
+
+                color: 'white',
+                fontFamily: 'Cabin',
+              }}>
+              <FontAwesomeIcon
+                icon={app.icon}
+                inverse
+                style={{paddingLeft: '5px'}}
+              />
+
+              <span style={{paddingLeft: '5px'}}>{app.title}</span>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
 
-interface App {
-  title: string;
-  content: string;
-}
-
-type AppAction = ['close' | 'open', App];
+type AppAction = ['close' | 'open' | 'minimize' | 'show', App];
 
 const Home = () => {
   const [apps, dispatch] = React.useReducer(
     (apps: App[], [type, target]: AppAction) => {
       switch (type) {
         case 'close':
-          return apps.filter((app) => app !== target);
+          return apps.filter((app) => app.id !== target.id);
         case 'open':
           return [...apps, target];
+        case 'minimize':
+          return apps.map((app) =>
+            app.id === target.id ? app.minimize() : app
+          );
+        case 'show':
+          return apps.map((app) => (app.id === target.id ? app.show() : app));
         default:
           return apps;
       }
     },
-    [] as App[]
+    [App.fromContent('Hello world', faTvRetro, 'Ryan was here')] as App[]
   );
 
   return (
-    <Desktop>
-      {apps.map((app) => {
-        return (
-          <Window title={app.title} onClose={() => dispatch(['close', app])}>
-            <p>{app.content}</p>
-          </Window>
-        );
-      })}
-
-      <DesktopShortcut
-        title="Hello world"
-        icon={faTvRetro}
-        action={() => dispatch(['open', {title: 'New', content: 'Hey!'}])}
-      />
-
-      <DesktopShortcut
-        title="Hello world"
-        icon={faTvRetro}
-        action={() => dispatch(['open', {title: 'New', content: 'Hey!'}])}
-      />
-    </Desktop>
+    <Desktop
+      apps={apps}
+      shortcuts={[{title: 'Hello world', icon: faTvRetro}]}
+      onOpenApp={(shortcut) => {
+        dispatch([
+          'open',
+          App.fromContent(shortcut.title, shortcut.icon, 'Ryan was here'),
+        ]);
+      }}
+      onCloseApp={(app) => {
+        dispatch(['close', app]);
+      }}
+      onMinimizeApp={(app) => {
+        dispatch(['minimize', app]);
+      }}
+      onShowApp={(app) => {
+        dispatch(['show', app]);
+      }}
+    />
   );
 };
 
