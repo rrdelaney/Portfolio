7d6eb74b7b633a7ae82133133632e91a51506b48
Ryan Delaney <rrdelaney@outlook.com>
2021-06-01 21:59:41 -0700

    Next.js

--- .eleventy.js
+++
@@ -1,3 +0,0 @@
-module.exports = function (eleventyConfig) {
-  eleventyConfig.setTemplateFormats(['md', 'njk', 'png']);
-};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
 module.exports = {
-  purge: ['./_site/**/*.html'],
+  purge: ['pages/**/*.{ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            'line-height': 1.5,
+            h1: {
+              color: theme('colors.indigo.500'),
+              'font-weight': theme('fontWeight.bold'),
+              'font-size': theme('fontSize.3xl'),
+            },
+          },
+        },
+      }),
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
 };
